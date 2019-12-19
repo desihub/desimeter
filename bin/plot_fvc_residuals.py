e13a5955465844bf28e0fca66e9515c0c7472f12
Julien Guy <jguy@lbl.gov>
2019-12-19 13:40:40 -0800

    add patch of metrology

--- bin/plot_fvc_residuals.py
+++ bin/plot_fvc_residuals.py
@@ -32,12 +32,13 @@ ym = table["YMETRO"][ii]
 a = plt.subplot(1,1,1)
 a.set_title(os.path.basename(filename))
 
+plt.plot(table["XFP"],table["YFP"],".")
 
 # plotting all of FIF and GIF
 filename = resource_filename('desimeter',"data/fp-metrology.csv")
 metrology = Table.read(filename,format="csv")
 selection=(metrology["Device Type"]=="FIF")|(metrology["Device Type"]=="GIF")
-plt.plot(metrology["XFP"][selection],metrology["YFP"][selection],"o",c="gray")
+plt.plot(metrology["XFP"][selection],metrology["YFP"][selection],"o",c="gray",alpha=0.2)
 
 plt.plot(x,y,"o")
 plt.plot(xm,ym,"X")
@@ -61,5 +62,84 @@ if jj.size > 0 : # sadly
 plt.quiver(x,y,dx,dy)
 plt.xlabel("XFP (mm)")
 plt.ylabel("YFP (mm)")
+
+
+if False :
+    print("Write coordinates of missing or incorrect fiducials")
+    x=table["XFP"]
+    y=table["YFP"]
+
+    xc=-289
+    yc=217
+    device_type="GIF"
+    petal_id=11
+    petal_loc=6
+    device_loc=999 # made up
+    location=1000*petal_loc+device_loc
+    print(table.dtype.names)
+    
+    # find nearests
+    ii=np.where((x-xc)**2+(y-yc)**2<5**2)[0]
+    #col0,Petal ID,Petal Loc ID,Device Loc ID,Device Type,DOTID,XPL,YPL,ZPL,X MNT,Y MNT,Z MNT,FCL Source,Proj Distance,Provenance,Notes,XFP,YFP,ZFP,LOCATION
+    j=np.argmin((x-xc)**2+(y-yc)**2)
+    line=10000
+    c=1
+    for i in ii :
+        if i==j :
+            dot=4 # center
+        else :
+            dot=c
+            c+=1
+            if c==4 : c+= 1
+            
+        print(x[i],y[i])
+        print("{line},{petal_id},{petal_loc},{device_loc},{device_type},{dot},0,0,0,0,0,0,'interpolated',0,'none','none',{x},{y},0,{location}".format(line=line,petal_id=petal_id,petal_loc=petal_loc,device_loc=device_loc,device_type=device_type,dot=dot,x=x[i],y=y[i],location=location))
+        line += 1
+
+    xc=194.7
+    yc=248.4
+    # find nearests
+    ii=np.where((x-xc)**2+(y-yc)**2<2**2)[0]
+    for i in ii :
+        print(table[:][i])
+    
+    device_type="FIF"
+    petal_id=8
+    petal_loc=4
+    device_loc=321 # made up
+    location=4321
+    dot=5 # new
+    print(table.dtype.names)
+    j=np.argmin((x-xc)**2+(y-yc)**2)
+    print("{line},{petal_id},{petal_loc},{device_loc},{device_type},{dot},0,0,0,0,0,0,'interpolated',0,'patch','none',{x},{y},0,{location}".format(line=line,petal_id=petal_id,petal_loc=petal_loc,device_loc=device_loc,device_type=device_type,dot=dot,x=x[j],y=y[j],location=location))
+    line += 1
+
+
+    # replacements
+    for (xc,yc) in [ (-47.4,41.7) , ( 367.6,176.7) ] :
+        
+        ii=np.where((x-xc)**2+(y-yc)**2<2**2)[0]
+
+        for i in ii :
+            selection=(metrology["LOCATION"]==table["LOCATION"][i])
+            for k in ["DOTID"] :
+                selection &= (metrology[k]==table[k][i])
+                jj=np.where(selection)[0]
+                if jj.size == 0 :
+                    print("no match???????")
+                    continue
+                j=jj[0]
+                print("X {} -> {}".format(metrology["XFP"][j],table["XFP"][i]))
+                print("Y {} -> {}".format(metrology["YFP"][j],table["YFP"][i]))
+                metrology["XFP"][j] = table["XFP"][i]
+                metrology["YFP"][j] = table["YFP"][i]
+                line="{} : ".format(i)
+                for k in metrology.dtype.names :
+                    line += ",{}".format(metrology[k][j])
+            print(line)
+    
+    
+
 plt.show()
+
 
--- bin/write_focal_plane_metrology.py
+++ bin/write_focal_plane_metrology.py
@@ -32,7 +32,11 @@ filename = resource_filename('desimeter',"data/UMT-DESI-5421-v1.csv")
 log.info(" in {}".format(filename))
 spots = Table.read(filename,format="csv")
 r=np.sqrt(spots["X FCL"]**2+spots["Y FCL"]**2)
+ii=np.where( r < 1.)[0]
+for i in ii :
+    log.warning("BAD DATA {} {} type={} xflc={} yflc={} xmnt={} ymnt={}".format(spots["Petal Loc ID"][i],spots["Device Loc ID"][i],spots["Device Type"][i],spots["X FCL"][i],spots["Y FCL"][i],spots["X MNT"][i],spots["Y MNT"][i]))
 spots=spots[:][r>1.] # exclude bad data??
+
 spots = apply_pl2fp(spots,petal_alignment_dict)
 
 spots["LOCATION"] = spots["Petal Loc ID"]*1000+spots["Device Loc ID"]
@@ -40,6 +44,24 @@ if 'Dot ID' in spots.dtype.names :
     spots.rename_column('Dot ID', 'DOTID')
     log.warning("rename_column('Dot ID', 'DOTID')")
 
+log.info("applying patch")
+filename = resource_filename('desimeter',"data/fp-metrology-patch.csv")
+log.info(" from {}".format(filename))
+patch = Table.read(filename,format="csv")
+for i in range(patch["XFP"].size) :
+    selection=(spots["Petal Loc ID"]==patch["Petal Loc ID"][i])
+    for k in ["Device Loc ID","DOTID"] :
+        selection &= (spots[k]==patch[k][i])
+    jj=np.where(selection)[0]
+    if jj.size == 0 :
+        log.info("Adding LOCATION={} DOTID={}".format(patch["LOCATION"][i],patch["DOTID"][i]))
+        spots.add_row(patch[:][i])
+    else :
+        j=jj
+        log.info("Replacing LOCATION={} DOTID={}".format(patch["LOCATION"][i],patch["DOTID"][i]))
+        spots[:][j] = patch[:][i]
+
+        
 spots.write(args.outfile,format='csv',overwrite=True)
 
 log.info("wrote {}".format(args.outfile))
