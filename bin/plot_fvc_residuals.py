b35a20acb7945459fee72454795a0a9b235abd30
Julien Guy <jguy@lbl.gov>
2019-12-18 16:35:46 -0800

    large residuals

--- bin/plot_fvc_residuals.py
+++ bin/plot_fvc_residuals.py
@@ -6,6 +6,7 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table
+from pkg_resources import resource_filename
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                      description="""Plot FVC spots, showing residuals with metrology""")
@@ -28,19 +29,35 @@ y = table["YFP"][ii]
 xm = table["XMETRO"][ii]
 ym = table["YMETRO"][ii]
 
-print(xm)
-
 a = plt.subplot(1,1,1)
 a.set_title(os.path.basename(filename))
 
-plt.plot(x,y,".")
-plt.plot(xm,ym,".")
 
-for i in range(x.size) :
-    plt.plot([x[i],xm[i]],[y[i],ym[i]],"-",c="gray",alpha=0.5)
+# plotting all of FIF and GIF
+filename = resource_filename('desicoord',"data/fp-metrology.csv")
+metrology = Table.read(filename,format="csv")
+selection=(metrology["Device Type"]=="FIF")|(metrology["Device Type"]=="GIF")
+plt.plot(metrology["XFP"][selection],metrology["YFP"][selection],"o",c="gray")
+
+plt.plot(x,y,"o")
+plt.plot(xm,ym,"X")
 
 dx=xm-x
 dy=ym-y
+
+jj=np.where((np.abs(dx)>0.1)|(np.abs(dy)>0.1))[0]
+if jj.size > 0 : # sadly
+    print("Large residuals")
+    for j in jj :
+        i=ii[j]
+        line = "dx={:4.3f}mm dy={:4.3f}mm".format(dx[j],dy[j])
+        for k in table.dtype.names :
+            if k=="XERR" or k=="YERR" : continue
+            line += " {}={:4.3f}".format(k,table[k][i])
+        print(line)
+        plt.plot(x[j],y[j],"+",color="red")
+
+
 plt.quiver(x,y,dx,dy)
 plt.xlabel("XFP (mm)")
 plt.ylabel("YFP (mm)")
