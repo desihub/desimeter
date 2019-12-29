c57fbc74b211d7fff8d569a1da282eee5988541d
Stephen Bailey <stephen.code@gmail.com>
2019-12-28 21:05:22 -0800

    test detecspots; remove faint and duplicate test spots

---
+++ py/desimeter/test/test_detectspots.py
@@ -0,0 +1,59 @@
+import unittest
+from pkg_resources import resource_filename
+from collections import Counter
+
+import numpy as np
+from scipy import ndimage
+from scipy.spatial import cKDTree as KDTree
+from astropy.table import Table
+
+from desimeter import detectspots
+from desimeter.transform.fvc2fp.poly2d import FVCFP_Polynomial
+from desimeter.transform.fvc2fp.zb import FVCFP_ZhaoBurge
+
+class TestDetectSpots(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        '''
+        Create test image based upon input spots
+        '''
+        cls.spotfile = resource_filename('desimeter', 'test/data/test-spots.csv')
+        cls.spots = spots = Table.read(cls.spotfile)
+        np.random.seed(1)
+        cls.img = np.random.normal(loc=1000.0, scale=35, size=(6000, 6000))
+        
+        spot = np.zeros((15,15))
+        spot[7,7] = 1.0
+        spot = detectspots.gaussian_convolve(spot)
+        for x, y, counts in zip(spots['XPIX'], spots['YPIX'], spots['COUNTS']):
+            dx = x % 1
+            dy = y % 1
+            x = int(x)
+            y = int(y)
+            cls.img[y-7:y+8, x-7:x+8] += counts*ndimage.shift(spot, (dy,dx))
+
+    def setUp(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_detect(self):
+        spots = detectspots.detectspots(self.img)
+        self.assertTrue(len(spots) == len(self.spots))
+
+        tree = KDTree(np.array((self.spots['XPIX'], self.spots['YPIX'])).T)
+        distances, indices = tree.query(np.array((spots['XPIX'], spots['YPIX'])).T)
+
+        #- all spots were matched
+        self.assertEqual(len(set(indices)), len(indices))
+        
+        #- loose check on distances because original image wasn't constructed
+        #- very exactly either; just check if we matched the right spot
+        self.assertLess(np.max(distances), 0.02)
+
+if __name__ == '__main__':
+    unittest.main()
+        
