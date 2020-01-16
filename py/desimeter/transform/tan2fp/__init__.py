c19c2acb97898f5df8383c1bc12a68607f7efc22
Stephen Bailey <stephen.code@gmail.com>
2020-01-16 13:37:12 -0800

    tangent plane to focal plane transforms

---
+++ py/desimeter/transform/tan2fp/__init__.py
@@ -0,0 +1,12 @@
+"""
+tan2fp: transformations between tangent plane and focal plane xy [mm]
+
+given spherical coordinate angles theta (polar), phi (azimuthal)::
+
+    xtan = sin(theta)*cos(phi)
+    ytan = sin(theta)*sin(phi)
+
+phi=0 is aligned with +xtan = +RA = -xfp in DESI
+"""
+
+from .echo22 import tan2fp, fp2tan
