183cc8aa86d03b624e7a1679de460b77e1ef2f65
Stephen Bailey <stephen.code@gmail.com>
2020-01-17 16:33:11 -0800

    stringent roundtrip test

--- py/desimeter/test/test_gfa2fp.py
+++ py/desimeter/test/test_gfa2fp.py
@@ -16,8 +16,8 @@ class TestTan2FP(unittest.TestCase):
         metrology = metrology[ii]
         
         nx, ny = 2048, 1032
-        xgfa_ref = np.array([0, nx-1, nx-1, 0])
-        ygfa_ref = np.array([0, 0, ny-1, ny-1])
+        xgfa_ref = np.array([0.0, nx-1, nx-1, 0])
+        ygfa_ref = np.array([0.0, 0, ny-1, ny-1])
         
         for p in range(10):
             ii = (metrology['PETAL_LOC'] == p)
@@ -25,16 +25,32 @@ class TestTan2FP(unittest.TestCase):
                 print('ERROR: missing GFA metrology for PETAL_LOC {}'.format(p))
                 continue
             
-            xfp = np.asarray(metrology['X_FP'][ii])
-            yfp = np.asarray(metrology['Y_FP'][ii])
+            xfp_ref = np.asarray(metrology['X_FP'][ii])
+            yfp_ref = np.asarray(metrology['Y_FP'][ii])
             
-            xgfa, ygfa = fp2gfa(p, xfp, yfp)
+            #- Current agreement is not very good, because the GFA metrology
+            #- is rectangular in 3D space but these transforms are treating
+            #- them as rectangular in 2D space.
+
+            xgfa, ygfa = fp2gfa(p, xfp_ref, yfp_ref)
+            self.assertLess(np.max(np.abs(xgfa-xgfa_ref)), 2)
+            self.assertLess(np.max(np.abs(ygfa-ygfa_ref)), 2)
             
-            if p == 0:
-                #--- DEBUG ---
-                import IPython
-                IPython.embed()
-                #--- DEBUG ---
+            xfp, yfp = gfa2fp(p, xgfa_ref, ygfa_ref)            
+            dxfp = xfp-xfp_ref
+            dyfp = yfp-yfp_ref
+            self.assertLess(np.max(np.abs(dxfp)), 0.050)
+            self.assertLess(np.max(np.abs(dyfp)), 0.050)
+
+            drfp = 1000*np.sqrt(dxfp**2 + dyfp**2)
+            print('PETAL_LOC {} GFA corner max(dr) = {:.1f} um'.format(
+                p, np.max(drfp)))
+        
+        #- Round trip tests should be rock solid though
+        xfp, yfp = gfa2fp(0, xgfa_ref, ygfa_ref)
+        xgfa, ygfa = fp2gfa(0, xfp, yfp)
+        self.assertTrue(np.allclose(xgfa, xgfa_ref))
+        self.assertTrue(np.allclose(ygfa, ygfa_ref))
 
     def test_scale_rotation_offset(self):
         x1 = np.array([1.0, 2.0, 3.0, 4.0])
