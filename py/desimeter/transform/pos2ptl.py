2b2a0e8b5393888e758bf503c05e10c47faab7bf
joesilber <58699331+joesilber@users.noreply.github.com>
2020-04-16 20:01:03 -0700

    Create pos2ptl.py
    Just starting comments describing what I will put here

---
+++ py/desimeter/transform/pos2ptl.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+This module provides coordinate transformations between petal cartesian and
+positioner local coordinates.
+
+    x_ptl, y_ptl ... Units mm. Cartesian coordinates, relative to petal.
+                     Corresponds to ptlXY in postransforms.py.
+                     
+    x_pos, y_pos ... Units mm. Cartesian coordinates, relative to an individual
+                     fi
+                     
+    poslocXY
+    flatXY
+    poslocTP
+    posintTP
+"""
+
+# Start with just the transforms I used in fitter.py.
+# Then work outward if desired.
+
+
