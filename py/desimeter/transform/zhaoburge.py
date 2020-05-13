ba0f82f294b57b3d6d47eefe04d6334b6576abb9
Julien Guy <jguy@lbl.gov>
2020-05-13 10:26:40 -0700

    try to make codacy happy

--- py/desimeter/transform/zhaoburge.py
+++ py/desimeter/transform/zhaoburge.py
@@ -14,7 +14,7 @@ def factorial(n):
        return int(n*factorial(n-1))
     else:
        return 1
-       
+
 def convertNolltoBW(noll):
     # converts a Noll Zernike index to the B&W {n,m} doublet
     n = int(-0.5+np.sqrt(2*noll-1.75))
@@ -25,12 +25,12 @@ def convertNolltoBW(noll):
         m = 2*int(0.5*diff + 0.7)
     else:
         m = 2*int(0.5*diff + 1.2) - 1
-    if noll%2>0:  
+    if noll%2>0:
         m = -m
     return np.array([n, m])
 
 def convertWyanttoBW(wyant):
-    # converts a Wyant Zernike index to the B&W {n,m}   
+    # converts a Wyant Zernike index to the B&W {n,m}
     halfsum = int(np.sqrt(wyant))
     idif = int(wyant - halfsum**2)
     halfdif = int (idif/2)
@@ -38,8 +38,8 @@ def convertWyanttoBW(wyant):
     m = halfsum - halfdif
     if idif%2 >0:
         m = -m
-    return np.array([n, m]) 
-    
+    return np.array([n, m])
+
 def getZernFuncXY(nm, xnorm, ynorm):   # nm is the BornWolf index; BIG NINE:  #1
     # Here, xnorm and ynorm must lie within the unit circle
     rnorm = np.sqrt(xnorm*xnorm + ynorm*ynorm)
@@ -64,16 +64,16 @@ def getZernRadial(nm, rnorm):    # BIG NINE: #4
         coef = numer / denom
         term = coef*np.power(rnorm, expon)
         result = result + term
-    return result  
-    
-def getZernAngular(nm, theta): 
+    return result
+
+def getZernAngular(nm, theta):
     m = nm[1]    # B&W
     if m==0:
         return 1.
     if m>0:
         return np.cos(m*theta)
     m = np.abs(m)         # note this abs() function
-    return np.sin(m*theta)  
+    return np.sin(m*theta)
 
 def zernFormulaText(nm):           # BIG NINE: #8
     #---generates a text representation of the specified Zernike function--
@@ -85,7 +85,7 @@ def zernFormulaText(nm):           # BIG NINE: #8
     halfsum = (n+m)/2
     idif = n-m
     halfdif = int(idif/2)
-    
+
     #--first do the radial part----
     nterms = 0
     s = ''
@@ -112,12 +112,12 @@ def zernFormulaText(nm):           # BIG NINE: #8
             s = s + '^' + str(expon)
     if nterms>1 and m!=0:
         s = '('+ s + ')'
-        
+
     #--then do the azimuthal part, if any--------
     if m==0:
         return s
     strm = ''
-    if m>1:    
+    if m>1:
         strm = str(m)
     if needsine:
         s = s + '*sin(' + strm + 't)'
@@ -138,12 +138,12 @@ NCOEFS = 33  # available item coefs
 #--------Zernike-Noll Renormalization for AreaIntegral = Pi----------------
 
 squares = np.array([0, 1, 4, 4, 3, 6, 6, 8, 8, 8, 8,  5, 10, 10, 10, 10, 12, 12, 12, 12, 12, 12, 7, 14, 14, 14, 14, 14, 14, 16, 16, 16, 16, 16, 16, 16, 16, 9])
-# for Noll index =  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12, 13, 14, 15, 16, 17, 18, 19, 20, 21,22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37 
+# for Noll index =  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12, 13, 14, 15, 16, 17, 18, 19, 20, 21,22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37
 
 def normalizeArea(noll):
     # converts an edge=1 Zernike to an area=Pi normalized Zernike
     return np.sqrt(squares[noll])
-  
+
 def getZ(noll, x, y):
     return normalizeArea(noll) * getZernFuncXY(convertNolltoBW(noll), x, y)
 
@@ -165,12 +165,12 @@ def getZhaoBurgeXY(polids, coeffs, x, y):
     return dx, dy
 
 def getZhaoBurgeTerm(polid, x, y):
-    
+
     # Cartesian input x,y; cartesian output x, y, and label.
     # Calls getZ() and delivers area normalized ZB terms
-    if polid==0:   # case "S2"  r^0, keep; X translate; 
-        result =  getZ(1,x,y), 0.0,            'S2'   # parm=0/13 translation 
-    elif polid==1:   # case "S3"  r^0, keep; Y translate; 
+    if polid==0:   # case "S2"  r^0, keep; X translate;
+        result =  getZ(1,x,y), 0.0,            'S2'   # parm=0/13 translation
+    elif polid==1:   # case "S3"  r^0, keep; Y translate;
         result =  0.0, getZ(1,x,y),             'S3'   # parm=1/13 translation
     elif polid==2:   # case "S4"  magnify
         result =  rh*getZ(2,x,y), rh*getZ(3,x,y), 'S4'  # parm=2/13 dilatation
@@ -178,7 +178,7 @@ def getZhaoBurgeTerm(polid, x, y):
         result =  rh*getZ(3,x,y), rh*getZ(2,x,y), 'S5'  # n/a
     elif polid==4:   # case "S6"
         result =  rh*getZ(2,x,y), -rh*getZ(3,x,y), 'S6'  # n/a
-    elif polid==5:   # case "S7" 
+    elif polid==5:   # case "S7"
         result =  0.5*getZ(5,x,y), rh*getZ(4,x,y)-0.5*getZ(6,x,y), 'S7' # parm=3/13
     elif polid==6:   # case "S8"
         result =  rh*getZ(4,x,y)+0.5*getZ(6,x,y), 0.5*getZ(5,x,y), 'S8'  # parm=4/13
@@ -186,11 +186,11 @@ def getZhaoBurgeTerm(polid, x, y):
         result =  rh*getZ(5,x,y), rh*getZ(6,x,y), 'S9'      # parm=5/13 but TREFOIL: DROPPING THIS
     elif polid==8:   # case "S10"
         result =  rh*getZ(6,x,y), -rh*getZ(5,x,y), 'S10'    # n/a but is SISTER TO S9 !!!
-    elif polid==9:   # case "S11" 
+    elif polid==9:   # case "S11"
         result =  rh*getZ(8,x,y), rh*getZ(7,x,y), 'S11'   # parm=6/13
     elif polid==10:  # case "S12"
         result =  0.5*getZ(8,x,y)+0.5*getZ(10,x,y), -0.5*getZ(7,x,y)+0.5*getZ(9,x,y), 'S12' # n/a
-    elif polid==11:  # case "S13" 
+    elif polid==11:  # case "S13"
         result =  0.5*getZ(7,x,y)+0.5*getZ(9,x,y), 0.5*getZ(8,x,y)-0.5*getZ(10,x,y), 'S13'  # n/a
     elif polid==12:  # case "S14"
         result =  rh*getZ(10,x,y), -rh*getZ(9,x,y),  'S14'   # n/a
@@ -214,7 +214,7 @@ def getZhaoBurgeTerm(polid, x, y):
         result =  0.5*getZ(17,x,y)+0.5*getZ(19,x,y), 0.5*getZ(16,x,y)-0.5*getZ(18,x,y), 'S23'  # n/a
     elif polid==22:  # Case "S24"
         result =  0.5*getZ(16,x,y)+0.5*getZ(18,x,y), -0.5*getZ(17,x,y)+0.5*getZ(19,x,y), 'S24' # n/a
-    elif polid==23:  # Case "S25" 
+    elif polid==23:  # Case "S25"
         result =  0.5*getZ(19,x,y)+0.5*getZ(21,x,y), 0.5*getZ(18,x,y)-0.5*getZ(20,x,y), 'S25'  # n/a
     elif polid==24:  # Case "S26"
         result =  0.5*getZ(18,x,y)+0.5*getZ(20,x,y), -0.5*getZ(19,x,y)+0.5*getZ(21,x,y), 'S26'  # n/a
@@ -224,20 +224,20 @@ def getZhaoBurgeTerm(polid, x, y):
         result =  rh*getZ(20,x,y), -rh*getZ(21,x,y), 'S28'                # n/a
     elif polid==27:  #  case "T4"
         result =  rh*getZ(3,x,y), -rh*getZ(2,x,y), 'T4'                   # parm=9/13 rotation
-    elif polid==28:  # case "T7" 
+    elif polid==28:  # case "T7"
         result =  rh*getZ(4,x,y)-0.5*getZ(6,x,y), -0.5*getZ(5,x,y), 'T7'  # parm=10/13
     elif polid==29:  # case "T8"
         result =  0.5*getZ(5,x,y), -rh*getZ(4,x,y)-0.5*getZ(6,x,y), 'T8'  # parm=11/13; TWO MINUS SIGNS
-    elif polid==30:  # case "T11" 
+    elif polid==30:  # case "T11"
         result =  rh*getZ(7,x,y), -rh*getZ(8,x,y), 'T11'                  # parm=12/13
     elif polid==31:  # case "T12"
         result =  -0.5*getZ(7,x,y)+0.5*getZ(9,x,y), -0.5*getZ(8,x,y)-0.5*getZ(10,x,y), 'T12' # n/a
     elif polid==32:  # case "T13"
         result =  0.5*getZ(8,x,y)-0.5*getZ(10,x,y), -0.5*getZ(7,x,y)-0.5*getZ(9,x,y), 'T13' # n/a
-    else: 
-        print("ZhaoBurgeTerm() is exitting because unsatisfied polid = ", polid) 
+    else:
+        print("ZhaoBurgeTerm() is exitting because unsatisfied polid = ", polid)
         quit()
-    return result   
+    return result
 
 #---------end of Zhao-Burge evaluator------------------------
 
@@ -265,32 +265,6 @@ def transform(x, y, scale, rotation, offset_x, offset_y, zbpolids=None, zbcoeffs
 
     return xx, yy
 
-def fit_scale_rotation_offset(x, y, xp, yp, fitzb=False, polids=None):
-    """
-    Fit scale, rotation, offset plus optional Zhao-Burge corrections
-    for x,y -> xp,yp.
-
-    TODO: document details
-    """
-
-    def func(params, x, y, xp, yp):
-        scale, rotation, offset_x, offset_y = params[0:4]
-        xx, yy = transform(x, y, scale, rotation, offset_x, offset_y)
-        dr2 = np.sum((xx-xp)**2 + (yy-yp)**2)
-        return dr2
-    
-    p0 = np.array([1.0,0.0,0.0,0.0])
-    p  = minimize(func, p0, args=(x, y, xp, yp))
-
-    scale, rotation, offset_x, offset_y = p.x
-
-    if fitzb :
-        xx, yy = transform(x, y, scale, rotation, offset_x, offset_y)
-        zbpolids, zbcoeffs, zbx, zby = fitZhaoBurge(xx, yy, xp, yp, polids)
-        return scale, rotation, offset_x, offset_y, zbpolids, zbcoeffs
-    else:
-        return scale, rotation, offset_x, offset_y
-    
 def fit_scale_rotation_offset(x, y, xp, yp, fitzb=False, zbpolids=None, zbcoeffs=None):
     """
     Fit scale, rotation, offset plus optional Zhao-Burge corrections
@@ -307,13 +281,13 @@ def fit_scale_rotation_offset(x, y, xp, yp, fitzb=False, zbpolids=None, zbcoeffs
     # the input coordinates.
 
     # however the first if just a fit of scale, rotation, offset_x, offset_y
-    
+
     def func(params, x, y, xp, yp):
         scale, rotation, offset_x, offset_y = params[0:4]
         xx, yy = transform(x, y, scale, rotation, offset_x, offset_y, zbpolids=zbpolids, zbcoeffs=zbcoeffs)
         dr2 = np.sum((xx-xp)**2 + (yy-yp)**2)
         return dr2
-    
+
     p0 = np.array([1.0,0.0,0.0,0.0])
     p  = minimize(func, p0, args=(x, y, xp, yp))
 
@@ -322,7 +296,7 @@ def fit_scale_rotation_offset(x, y, xp, yp, fitzb=False, zbpolids=None, zbcoeffs
     if fitzb :
 
         # here we refit only the ZB coefficients, but those should include provision
-        # for adjustment of shift translation rotation 
+        # for adjustment of shift translation rotation
         xx, yy = transform(x, y, scale, rotation, offset_x, offset_y)
         zbpolids, zbcoeffs, zbx, zby = fitZhaoBurge(xx, yy, xp, yp, zbpolids)
         return scale, rotation, offset_x, offset_y, zbpolids, zbcoeffs
@@ -340,10 +314,10 @@ def fitZhaoBurge(x, y, xp, yp, polids=None):
     # 1  = S3 = translation along Y
     # 2  = S4 = magnification
     # 27 = T4 = rotation
-    
+
     if polids is None :
         polids = np.array([0,1,2,3,4,5,6,9,20,27,28,29,30],dtype=int)
-    
+
     nzb = polids.size
     H = np.zeros((2*nx, nzb))
     for i,polid in enumerate(polids) :
