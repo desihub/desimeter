2d85374353c071a52cc6dad3d689400f8584d40b
Julien Guy <jguy@lbl.gov>
2020-05-20 10:37:52 -0700

    add LOG_NOTE selection

---
+++ py/desimeter/posparams/posmoveselection.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def posmove_selection(table,log_note_selection=None) :
+    """
+    select entries in input table and return table with selected rows
+    """
+    if log_note_selection is None : return table
+    keywords=[]
+    for v in log_note_selection.split("&") :
+        v = v.strip()
+        if len(v)>0 : keywords.append(v)
+    print("Posmove selection keywords: {}".format(keywords))
+
+    ok=(table["OFFSET_T"]!=0)
+    for keyword in keywords :
+        for i in range(len(table)) :
+            ok[i] &= (table["LOG_NOTE"][i].find(keyword)>=0)
+
+    print("Unique values of LOG_NOTE after selection: \n{}".format(np.unique(table["LOG_NOTE"][ok])))
+    print("Number of selected entries = {}".format(np.sum(ok)))
+
+    return table[ok]
