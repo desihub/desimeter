a91ee17948f9cfef429c49ed77504488cf17c6e5
Dustin Lang <dstndstn@gmail.com>
2020-05-29 15:40:22 -0400

    add scripts for running & summarizing all guide sequences on a date

---
+++ bin/run-date.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -e
+
+date=$1
+mkdir -p $date
+for fn in GFA/reduced/v0005_guide/2020${date}/*/guide-*_catalog-00002.fits; do
+    echo $fn
+    E=$(echo $fn | awk 'BEGIN{FS="/"} {print $5}')
+    echo "Exposure $E"
+    outdir=${date}/
+    echo "Processing $fn"
+    python3 ../bin/desi_fit_guide_star_coordinates -i $fn -o ${outdir}fm-$E.json --cat ${outdir}fm$E.fits;
+done
