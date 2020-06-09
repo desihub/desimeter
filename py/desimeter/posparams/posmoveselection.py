2c5add00a43a41239110437ba767dd4de26acf45
joesilber <58699331+joesilber@users.noreply.github.com>
2020-06-09 11:11:13 -0700

    add OR operation for log_note_selection

--- py/desimeter/posparams/posmoveselection.py
+++ py/desimeter/posparams/posmoveselection.py
@@ -6,18 +6,23 @@ def posmove_selection(table,log_note_selection=None) :
     """
     if log_note_selection is None : return table
     print("Unique values of LOG_NOTE before selection: \n{}".format(np.unique(table["LOG_NOTE"])))
-    keywords=[]
-    for v in log_note_selection.split("&") :
-        v = v.strip()
-        if len(v)>0 : keywords.append(v)
-    print("Posmove selection keywords: {}".format(keywords))
-
-    ok=np.repeat(True,len(table))
-    for keyword in keywords :
-        for i in range(len(table)) :
-            ok[i] &= (str(table["LOG_NOTE"][i]).find(keyword)>=0)
-
+    selection_strs = log_note_selection.split('|')
+    ok = np.array([False for i in range(len(table))])
+    for s in selection_strs:
+        ok |= get_matches(table, s)
     print("Unique values of LOG_NOTE after selection: \n{}".format(np.unique(table["LOG_NOTE"][ok])))
     print("Number of selected entries = {}".format(np.sum(ok)))
 
     return table[ok]
+
+def get_matches(table, selection_str):
+    keywords=[]
+    for v in selection_str.split("&") :
+        v = v.strip()
+        if len(v)>0 : keywords.append(v)
+    print("Posmove selection keywords: {}".format(keywords))
+    ok=np.repeat(True,len(table))
+    for keyword in keywords :
+        for i in range(len(table)) :
+            ok[i] &= (str(table["LOG_NOTE"][i]).find(keyword)>=0)
+    return ok
