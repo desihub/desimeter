d64db28706d3de1d420297adb8aa722805bf0ec1
Julien Guy <jguy@lbl.gov>
2020-09-02 09:52:23 -0700

    use location instead of device_id as spotmatch index, use tempfile, add info from metrology and add dummy pinhole_ids for fiducials

--- py/desimeter/spotmatch.py
+++ py/desimeter/spotmatch.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 import subprocess
 import shutil
+import tempfile
 
 from astropy.table import Table
 
@@ -33,9 +34,6 @@ def _compute_pixel_scale(fvc2fp) :
     return np.sqrt(np.abs(np.linalg.det(J))) # mm per pixel
 
 
-def _device_id_to_int(device_id) :
-    return int(str(device_id).replace("M","").replace("P",""))
-
 def _write_spotmatch_fiducial_config_file(filename) :
 
 
@@ -47,12 +45,12 @@ def _write_spotmatch_fiducial_config_file(filename) :
     metrology = load_metrology()
 
     selection  = (metrology["DEVICE_TYPE"]=="FIF")|(metrology["DEVICE_TYPE"]=="GIF")
-    device_ids = np.unique(metrology["DEVICE_ID"][selection])
+    locations = np.unique(metrology["LOCATION"][selection])
 
     with open(filename,"w") as ofile :
 
-        for device_id in device_ids :
-            selection = (metrology["DEVICE_ID"]==device_id)
+        for location in locations :
+            selection = (metrology["LOCATION"]==location)
             pinid     = metrology["PINHOLE_ID"][selection]
             xfp       = metrology["X_FP"][selection]
             yfp       = metrology["Y_FP"][selection]
@@ -61,13 +59,12 @@ def _write_spotmatch_fiducial_config_file(filename) :
             dx = xfp-mx
             dy = yfp-my
 
-            device_id_int = _device_id_to_int(device_id)
-            ofile.write("{} {:4.3f} {:4.3f} 000.0 0.001 {:05d} 8\n".format(device_id_int,0.,0.,0))
+            ofile.write("{} {:4.3f} {:4.3f} 000.0 0.001 {:05d} 8\n".format(location,0.,0.,0))
             for i in range(dx.size) :
-                ofile.write("{} {:4.3f} {:4.3f} 000.0 0.001 {:05d} 2\n".format(device_id_int,dx[i],dy[i],pinid[i]))
+                ofile.write("{} {:4.3f} {:4.3f} 000.0 0.001 {:05d} 2\n".format(location,dx[i],dy[i],pinid[i]))
     print("wrote",filename)
 
-def _write_spotmatch_targets_file(x_fp,y_fp,device_id,filename,fvc2fp=None) :
+def _write_spotmatch_targets_file(x_fp,y_fp,location,filename,fvc2fp=None) :
     """
     example:
 
@@ -86,20 +83,20 @@ def _write_spotmatch_targets_file(x_fp,y_fp,device_id,filename,fvc2fp=None) :
 
     x_fp = np.array(x_fp)
     y_fp = np.array(y_fp)
-    device_id = np.array(device_id)
+    location = np.array(location)
     flags = np.repeat(4,x_fp.size)
 
     # add fiducials centers
     metrology = load_metrology()
     selection  = (metrology["DEVICE_TYPE"]=="FIF")|(metrology["DEVICE_TYPE"]=="GIF")
-    fid_device_ids = np.unique(metrology["DEVICE_ID"][selection])
-    for fid_device_id in fid_device_ids :
-        selection = (metrology["DEVICE_ID"]==fid_device_id)
+    fid_locations = np.unique(metrology["LOCATION"][selection])
+    for fid_location in fid_locations :
+        selection = (metrology["LOCATION"]==fid_location)
         mx = np.mean(metrology["X_FP"][selection])
         my = np.mean(metrology["Y_FP"][selection])
         x_fp = np.append(x_fp,mx)
         y_fp = np.append(y_fp,my)
-        device_id = np.append(device_id,fid_device_id)
+        location = np.append(location,fid_location)
         flags = np.append(flags,8)
 
 
@@ -107,8 +104,7 @@ def _write_spotmatch_targets_file(x_fp,y_fp,device_id,filename,fvc2fp=None) :
 
     with open(filename,"w") as ofile :
         for i in range(xpix.size) :
-            device_id_int = _device_id_to_int(device_id[i])
-            ofile.write("{} {:4.3f} {:4.3f} 12.000 0.001 {}\n".format(device_id_int,xpix[i],ypix[i],flags[i]))
+            ofile.write("{} {:4.3f} {:4.3f} 12.000 0.001 {}\n".format(location[i],xpix[i],ypix[i],flags[i]))
     print("wrote",filename)
 
 
@@ -153,19 +149,18 @@ def _write_spotmatch_reference_pos_file(filename,fvc2fp=None) :
 
     selection = (metrology["DEVICE_TYPE"]=="FIF")|(metrology["DEVICE_TYPE"]=="GIF")
     xpix,ypix   = fvc2fp.fp2fvc(metrology["X_FP"][selection],metrology["Y_FP"][selection])
-    device_ids  = metrology["DEVICE_ID"][selection]
+    locations  = metrology["LOCATION"][selection]
     pinhole_ids = metrology["PINHOLE_ID"][selection]
 
     num={1:515,2:1027,3:259,4:771} # the code wants that
 
     with open(filename,"w") as ofile :
         for i in range(xpix.size) :
-            device_id_int = _device_id_to_int(device_ids[i])
-            ofile.write(" {} {:4.3f} {:4.3f} 12.000 {} 2.000 pinhole\n".format(device_id_int,xpix[i],ypix[i],num[pinhole_ids[i]]))
+            ofile.write(" {} {:4.3f} {:4.3f} 12.000 {} 2.000 pinhole\n".format(locations[i],xpix[i],ypix[i],num[pinhole_ids[i]]))
 
     print("wrote",filename)
 
-def spotmatch(xpix,ypix,expected_x_fp=None,expected_y_fp=None,expected_device_id=None,verbose=0,match_radius_pixels=50) :
+def spotmatch(xpix,ypix,expected_x_fp=None,expected_y_fp=None,expected_location=None,verbose=0,match_radius_pixels=70) :
     """
 
     """
@@ -183,13 +178,13 @@ def spotmatch(xpix,ypix,expected_x_fp=None,expected_y_fp=None,expected_device_id
     fid_x_dir=-1
     fid_y_dir=1
 
-    tmp_dir="."
+    tmp_dir=tempfile.gettempdir()
 
     fvc2fp = FVC2FP.read(fvc2fp_filename())
     exp_pixel_scale = _compute_pixel_scale(fvc2fp)
 
 
-    fiducial_config_filename=os.path.join(tmp_dir,"match_fiducials.test")
+    fiducial_config_filename=os.path.join(tmp_dir,"desimeter_spotmatch_fiducials.txt")
     _write_spotmatch_fiducial_config_file(fiducial_config_filename)
 
     if expected_x_fp is None :
@@ -198,16 +193,16 @@ def spotmatch(xpix,ypix,expected_x_fp=None,expected_y_fp=None,expected_device_id
         spots = metrology[metrology["DEVICE_TYPE"]=="POS"] # select positioners
         expected_x_fp = spots["X_FP"]
         expected_y_fp = spots["Y_FP"]
-        expected_device_id = spots["DEVICE_ID"]
+        expected_location = spots["LOCATION"]
 
 
-    targets_filename=os.path.join(tmp_dir,"match_targets.test")
-    _write_spotmatch_targets_file(expected_x_fp,expected_y_fp,expected_device_id,targets_filename,fvc2fp)
+    targets_filename=os.path.join(tmp_dir,"desimeter_spotmatch_targets.txt")
+    _write_spotmatch_targets_file(expected_x_fp,expected_y_fp,expected_location,targets_filename,fvc2fp)
 
-    measured_pos_filename=os.path.join(tmp_dir,"match_centroids.test")
+    measured_pos_filename=os.path.join(tmp_dir,"desimeter_spotmatch_input_centroids.txt")
     _write_spotmatch_measured_pos_file(xpix,ypix,measured_pos_filename)
 
-    reference_pos_filename=os.path.join(tmp_dir,"pinhole_references.test")
+    reference_pos_filename=os.path.join(tmp_dir,"desimeter_spotmatch_pinhole_references.txt")
     _write_spotmatch_reference_pos_file(reference_pos_filename,fvc2fp=None)
 
     """
@@ -220,8 +215,8 @@ easured_pos_file ./match_centroids.tmp -pos_save_file ./measured_pos.tmp -refere
 
     """
 
-    match_centers_filename=os.path.join(tmp_dir,"match_centers.test")
-    saved_pos_filename=os.path.join(tmp_dir,"saved_pos.test")
+    match_centers_filename=os.path.join(tmp_dir,"desimeter_spotmatch_output_centers.txt")
+    saved_pos_filename=os.path.join(tmp_dir,"desimeter_spotmatch_saved_output.txt")
 
     cmd =  "match_positions"
     cmd += " -verbose {}".format(verbose)
@@ -247,7 +242,7 @@ easured_pos_file ./match_centroids.tmp -pos_save_file ./measured_pos.tmp -refere
 
 
 
-    device_id=[]
+    location=[]
     xpix=[]
     ypix=[]
     mag=[]
@@ -259,7 +254,7 @@ easured_pos_file ./match_centroids.tmp -pos_save_file ./measured_pos.tmp -refere
         for line in ifile.readlines() :
             vals=line.strip().split()
             if len(vals)!=7 : continue
-            device_id.append(int(vals[0]))
+            location.append(int(vals[0]))
             xpix.append(float(vals[1]))
             ypix.append(float(vals[2]))
             mag.append(float(vals[3]))
@@ -269,18 +264,38 @@ easured_pos_file ./match_centroids.tmp -pos_save_file ./measured_pos.tmp -refere
     print("read {} entries in {}".format(len(xpix),match_centers_filename))
 
     res=Table()
-    res["DEVICE_ID"]=device_id
+    res["LOCATION"]=location
     res["XPIX"]=xpix
     res["YPIX"]=ypix
     res["FLAG"]=flag
     res["ERR"]=err
-    res["DEVICE_TYPE"]=np.repeat("unknown",len(res["DEVICE_ID"]))
-    # use metrology to get the actual device type back
+    nspots=len(location)
+    res["SPOTMATCH_DEVICE_TYPE"]=device_type
+
+    res["DEVICE_ID"]   = np.repeat("unknown",nspots)
+    res["DEVICE_TYPE"] = np.repeat("unknown",nspots)
+
+    # use metrology to get more info
     metrology = load_metrology()
-    dmap = {}
-    for i,did in enumerate(metrology["DEVICE_ID"]) :
-        dmap[_device_id_to_int(did)] = i
-    for j,did in enumerate(res["DEVICE_ID"]) :
-        if did in dmap :
-            res["DEVICE_TYPE"][j]=metrology["DEVICE_TYPE"][dmap[did]]
+    locmap = {loc:index for index,loc in enumerate(metrology["LOCATION"])}
+    matched_index   = np.where(res["LOCATION"]>=0)[0]
+    metrology_index = [locmap[loc] for loc in res["LOCATION"][matched_index]]
+    keys_to_copy = []
+    for k in ["DEVICE_TYPE","DEVICE_LOC","DEVICE_ID","PETAL_LOC","PETAL_ID","BUS_ID"] :
+        if k in metrology.dtype.names :
+            keys_to_copy.append(k)
+            if not k in res.dtype.names :
+                res[k]=np.repeat(-1,nspots)
+            res[k][matched_index] = metrology[k][metrology_index]
+
+    # special treatment for pinhole ids
+    res["PINHOLE_ID"]  = np.repeat(0,nspots)
+    fiducials=(res["SPOTMATCH_DEVICE_TYPE"]=="fiducial")
+
+    # dummy pinhole numbers. 99 for center, 10,11,12,13 for pinholes
+    res["PINHOLE_ID"][fiducials] = 99
+    for loc in np.unique(res["LOCATION"][fiducials]) :
+        ii = (res["LOCATION"]==loc)&(res["SPOTMATCH_DEVICE_TYPE"]=="pinhole")
+        res["PINHOLE_ID"][ii] = 10+np.arange(np.sum(ii))
+
     return res
