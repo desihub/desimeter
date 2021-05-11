1b4e3bd8e3fdc04f30b7072ea6826c59a8b86a41
Julien Guy <jguy@lbl.gov>
2021-05-11 13:39:11 -0700

    add inverse transform

--- py/desimeter/fiberassign.py
+++ py/desimeter/fiberassign.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-from desimeter.transform.radec2tan import radec2tan,hadec2xy
-from desimeter.transform.tan2fp.raytracefit import tan2fp
-from desimeter.transform.pos2ptl import ptl2flat
+from desimeter.transform.radec2tan import radec2tan,tan2radec,hadec2xy
+from desimeter.transform.tan2fp.raytracefit import tan2fp,fp2tan
+from desimeter.transform.pos2ptl import ptl2flat,flat2ptl
 from desimeter.trig import sincosd
 
 def _measure_fieldrot_deg(ha,dec,tel_ha,tel_dec,xfp_mm,yfp_mm) :
@@ -143,7 +143,91 @@ def fiberassign_radec2xy_cs5(ra,dec,tile_ra,tile_dec,tile_mjd,tile_ha,tile_field
 
     return xfp,yfp
 
-def fiberassign_radec2xy_flat(ra,dec,tile_ra,tile_dec,tile_mjd,tile_ha,tile_fieldrot,adc1,adc2) :
+def fiberassign_cs5_xy2radec(xfp,yfp,tile_ra,tile_dec,tile_mjd,tile_ha,tile_fieldrot,adc1=None,adc2=None) :
+    """Computes RA Dec from focal plane coordinates of targets in CS5 coordinate system (inverse of fiberassign_radec2xy_cs5)
+    Args:
+      xfp  : 1D numpy.array with target X in mm (need an array of points to compute the field rotation)
+      yfp : 1D numpy.array with target Y in mm
+      tile_ra : float, RA of tile center in degrees, such that after pointing and ADC shift corrections, this corresponds to X=0,Y=0
+      tile_dec : float, Dec of tile center in degrees, such that after pointing and ADC shift corrections, this corresponds to X=0,Y=0
+      tile_mjd : float, days, used for precession (does not need to be super accurate)
+      tile_ha :  float, hour angle of observation, in degrees
+      tile_fieldrot :  float, design/requested field rotation, in degrees
+      adc1 : optional, float, ADC1 angle in degrees: by default it is computed based on the HA and Dec.
+      adc2 : optional, float, ADC1 angle in degrees: by default it is computed based on the HA and Dec.
+
+    Returns RA,Dec, 1D numpy arrays of same size as X,Y
+    """
+
+    assert(xfp.size == yfp.size)
+
+    # LST from HA
+    lst=tile_ha+tile_ra
+
+    if ( (adc1 is None) and (adc2 is not None) ) or ( (adc1 is not None) and (adc2 is None) ) :
+        raise ValueError("either set both adc angles or none")
+
+    if adc1 is None :
+        adc1,adc2 =  pm_get_adc_angles(tile_ha,tile_dec)
+
+    # start with pointing = tile center
+    tel_ra=tile_ra+0.
+    tel_dec=tile_dec+0.
+
+    # tune telescope pointing given ADC angle
+    # in order to fp coordinates of tile RA and DEC, it's not zero because of the ADC angles
+    for iteration in range(2) :
+
+        xtan,ytan = radec2tan(np.array([tile_ra]),np.array([tile_dec]),tel_ra,tel_dec,tile_mjd,lst,hexrot_deg=0)
+        xfp_0,yfp_0   = tan2fp(xtan,ytan,adc1,adc2) #mm
+        #print("Temp tile center in FP coordinates = {},{} mm".format(xfp_0[0],yfp_0[0]))
+
+        # numeric derivative
+        eps = 1./3600. #
+        xtan,ytan = radec2tan(np.array([tile_ra+eps]),np.array([tile_dec]),tel_ra,tel_dec,tile_mjd,lst,hexrot_deg=0)
+        xfp_dra,yfp_dra   = tan2fp(xtan,ytan,adc1,adc2) #mm
+        xtan,ytan = radec2tan(np.array([tile_ra]),np.array([tile_dec+eps]),tel_ra,tel_dec,tile_mjd,lst,hexrot_deg=0)
+        xfp_ddec,yfp_ddec   = tan2fp(xtan,ytan,adc1,adc2) #mm
+        dxdra=(xfp_dra[0]-xfp_0[0])/eps
+        dydra=(yfp_dra[0]-yfp_0[0])/eps
+        dxddec=(xfp_ddec[0]-xfp_0[0])/eps
+        dyddec=(yfp_ddec[0]-yfp_0[0])/eps
+        J=[[dxdra,dxddec],[dydra,dyddec]]
+
+        # solve linear system to get tile RA Dec at center of fov
+        Jinv=np.linalg.inv(J)
+        X=Jinv.dot([xfp_0[0],yfp_0[0]])
+        dra=X[0]
+        ddec=X[1]
+
+        # apply offset to telescope pointing
+        tel_ra += dra
+        tel_dec += ddec
+
+    xtan,ytan = fp2tan(xfp,yfp,adc1,adc2)
+    tmp_ra,tmp_dec    = tan2radec(xtan,ytan,tel_ra,tel_dec,tile_mjd,lst,hexrot_deg=0)
+
+    # need to deal with field rotation
+    # measure field rotation
+    tmp_fieldrot = _measure_fieldrot_deg(-tmp_ra,tmp_dec,-tile_ra,tile_dec,xfp,yfp)
+
+    # apply field rotation to match request
+    drot = tile_fieldrot-tmp_fieldrot
+    s,c  = sincosd(drot)
+    tmp_xfp = c * xfp + s * yfp
+    tmp_yfp = -s * xfp + c * yfp
+
+    xtan,ytan      = fp2tan(tmp_xfp,tmp_yfp,adc1,adc2)
+    ra,dec = tan2radec(xtan,ytan,tel_ra,tel_dec,tile_mjd,lst,hexrot_deg=0)
+
+    # verify
+    realised_fieldrot = _measure_fieldrot_deg(-ra,dec,-tile_ra,tile_dec,xfp,yfp)
+    print("Requested fieldrot={:3.1f} arcsec delta={:3.1f} arcsec".format(tile_fieldrot*3600.,(tile_fieldrot-realised_fieldrot)*3600.))
+
+    return ra,dec
+
+
+def fiberassign_radec2xy_flat(ra,dec,tile_ra,tile_dec,tile_mjd,tile_ha,tile_fieldrot,adc1=None,adc2=None) :
     """Computes X Y focal plane coordinates of targets in the curved, "flat" coordinate system.
     Args:
       ra  : 1D numpy.array with target RA in degrees (need an array of points to compute the field rotation)
@@ -166,3 +250,25 @@ def fiberassign_radec2xy_flat(ra,dec,tile_ra,tile_dec,tile_mjd,tile_ha,tile_fiel
     xflat,yflat = ptl2flat(xfp,yfp)
 
     return xflat,yflat
+
+
+
+def fiberassign_flat_xy2radec(xflat,yflat,tile_ra,tile_dec,tile_mjd,tile_ha,tile_fieldrot,adc1=None,adc2=None) :
+    """Computes RA Dec from focal plane coordinates of targets in curved "flat" coordinate system (inverse of fiberassign_radec2xy_flat)
+    Args:
+      xflat  : 1D numpy.array with target RA in degrees (need an array of points to compute the field rotation)
+      yflat : 1D numpy.array with target Dec in degrees, same size as ra
+      tile_ra : float, RA of tile center in degrees, such that after pointing and ADC shift corrections, this corresponds to X=0,Y=0
+      tile_dec : float, Dec of tile center in degrees, such that after pointing and ADC shift corrections, this corresponds to X=0,Y=0
+      tile_mjd : float, days, used for precession (does not need to be super accurate)
+      tile_ha :  float, hour angle of observation, in degrees
+      tile_fieldrot :  float, design/requested field rotation, in degrees
+      adc1 : optional, float, ADC1 angle in degrees: by default it is computed based on the HA and Dec.
+      adc2 : optional, float, ADC1 angle in degrees: by default it is computed based on the HA and Dec.
+
+    Returns RA,Dec, 1D numpy arrays of same size as xflat,yflat
+    """
+
+    xfp,yfp = flat2ptl(xflat,yflat)
+    ra,dec  = fiberassign_cs5_xy2radec(xfp,yfp,tile_ra,tile_dec,tile_mjd,tile_ha,tile_fieldrot,adc1=None,adc2=None)
+    return ra,dec
